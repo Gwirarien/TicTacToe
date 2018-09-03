68ba76f956ae654629826e1cde6b70952a729e2f
Gwirarien <obangbranes@yahoo.com>
2018-09-03 21:45:44 +0300

    Deleted pictures from commit - will be added later

---
+++ TicTacToe/MainMenuState.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "State.h"
+#include "Game.h"
+
+class MainMenuState : public State
+{
+public:
+	MainMenuState(GameDataRef dataRef);
+
+	virtual void initializeState() override;
+	virtual void inputHandler() override;
+	virtual void update(double deltaFrames) override;
+	virtual void draw(double deltaFrames) override;
+
+private:
+	GameDataRef _data;
+	sf::Sprite _background;
+	sf::Sprite _playButton;
+	sf::Sprite _playButtonOuter;
+	sf::Sprite _title;
+
+	std::vector<std::string> _backgroundNames = {"GameTitle", "PlayButton", "PlayButtonOuter", "Background"};
+	std::vector<std::string> _backgroundPaths = { MAIN_MENU_PLAY_BUTTON_OUTER };
+};
